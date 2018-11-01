7a0d905cca72a7e0d718ac32a0d414b8bf8ed7cd
Wahyu Permadi <wahyu.permadi1725@gmail.com>
2018-11-01 15:57:47 +0800

    second push

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -2,9 +2,7 @@
   <footer class="footer hero">
     <div class="container">
       <div class="content has-text-centered">
-        <p>
-          <strong>Vuesume</strong> by Dennis Russell. The source code is licensed <a href="http://opensource.org/licenses/mit-license.php">MIT</a>, and available on Github<a href="https://github.com/rasserudenisu/vuesume"><i class="icon fa fa-github"></i></a>
-        </p>
+      
       </div>
     </div>
   </footer>
